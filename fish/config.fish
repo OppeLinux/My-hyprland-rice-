ba4ac293ede0c49eda00f4e2249fa24ea528c09a
OppeLinux <asusnenega@gmail.com>
2026-07-11 11:49:38 -0700

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+# or if you only want dark mode:
+fish_config theme choose catppuccin-frappe --color-theme=dark
+fish_config theme choose catppuccin-macchiato --color-theme=dark
+fish_config theme choose catppuccin-mocha --color-theme=dark
+fastfetch
+end
